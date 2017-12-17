a0229e6eb9275b0abca83788ce4c595cc8ad6e03
h54345 <30908756+h54345@users.noreply.github.com>
2017-12-18 00:48:13 +0530

    Myflaskappvideo1

---
+++ src/data.py
@@ -0,0 +1,28 @@
+def Articles():
+    articles =[
+        {
+        'id':1,
+        'title':'Article One',
+        'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam asperiores corporis fugit harum magnam nobis officiis praesentium quae repellendus temporibus. Asperiores dignissimos dolore eveniet exercitationem necessitatibus quasi quod! Dolore, ratione!',
+        'Author': 'Hamza Naseem',
+        'Created date':'04-4-2017'
+
+    },
+    {
+        'id': 2,
+        'title': 'Article two',
+        'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam asperiores corporis fugit harum magnam nobis officiis praesentium quae repellendus temporibus. Asperiores dignissimos dolore eveniet exercitationem necessitatibus quasi quod! Dolore, ratione!',
+        'Author': 'Hamza Naseem',
+        'Created date': '04-4-2017'
+
+    },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam asperiores corporis fugit harum magnam nobis officiis praesentium quae repellendus temporibus. Asperiores dignissimos dolore eveniet exercitationem necessitatibus quasi quod! Dolore, ratione!',
+            'Author': 'Hamza Naseem',
+            'Created date': '04-4-2017'
+
+        },
+    ]
+    return articles
