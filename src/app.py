613f474ccad83e0806d9be6cec29793e950382e6
h54345 <30908756+h54345@users.noreply.github.com>
2017-12-20 22:33:38 +0530

    Myflaskappvideo1complete

--- src/app.py
+++ src/app.py
@@ -1,24 +1,25 @@
 from flask import Flask,request,render_template,flash,redirect,session,url_for,logging
 from data import Articles
 from flaskext.mysql import MySQL
+from pymysql.cursors import DictCursor
+
+#from MySQLdb.cursors import DictCursor
 from wtforms import Form,StringField,TextAreaField,PasswordField,validators
 from passlib.hash import sha256_crypt
 
 #from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-mysql = MySQL()
-mysql.init_app(app)
-
-
-
+mysql = MySQL(cursorclass=DictCursor)
 #config MYSQL
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_PORT'] = 3306
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'pythondb'
-app.config['MYSQL_DATABASE_CURSORCLASS'] = 'DictCursor'
+#app.config['MYSQL_CURSOR'] = 'DictCursor'
+mysql.init_app(app)
+
 
 
 @app.route("/")
@@ -80,7 +81,40 @@ def register():
         redirect(url_for('index'))
     return render_template('register.html', form=form)
 
+ #User Login
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get Form Fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # Create cursor
+        cursor = mysql.get_db().cursor()
+
+        # Get user by username
+        result = cursor.execute("SELECT * FROM users WHERE username = %s", [username])
+
+        if result > 0:
+            # Get stored hash
+            row = cursor.fetchone()
+            password=row['password']
+
+
+            # Compare Passwords
+            if sha256_crypt.verify(password_candidate, password):
+                # Passed
+                app.logger.info('Password matched')
+            else:
+                app.logger.info('PASSWord not matched')
+        else:
+            app.logger.info('NO User')
+
+    return render_template('login.html')
+
 
 if __name__ == "__main__":
-    app.secret_key='Secret123'
+    app.secret_key = 'Secret123'
     app.run(debug=True)
